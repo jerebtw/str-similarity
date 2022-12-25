6723213b11a4d7135c2aaed6930a129a1450dff5
Jerebtw <jeremialp41@gmail.com>
2022-12-25 12:51:16 +0100

    Fixed algorithm and updated tests, benchmarks
    The implementation of the Levenshtein distance was not correct.
    Now it should give the correct result, but still between 0 and 1.
    The performance was also improved

--- benches/sim_benchmark.rs
+++ benches/sim_benchmark.rs
@@ -1,13 +1,33 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use str_similarity::compare_two_strings;
+use criterion::{criterion_group, criterion_main, Criterion};
+use str_similarity::{compare_two_strings, find_best_match};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("compares Nacht and Nacht", |b| {
-        b.iter(|| compare_two_strings("Nacht", "Night"))
-    });
-    c.bench_function("compares Night and Nacht", |b| {
-        b.iter(|| compare_two_strings(black_box("Nacht"), black_box("Nacht")))
-    });
+  c.bench_function("compares Nacht and Nacht", |b| {
+    b.iter(|| compare_two_strings("Nacht", "Night"))
+  });
+  c.bench_function("compares Night and Nacht", |b| {
+    b.iter(|| compare_two_strings("Nacht", "Nacht"))
+  });
+  c.bench_function("compares Night Night Night and Nacht Nacht Nacht", |b| {
+    b.iter(|| compare_two_strings("Night Night Night", "Nacht Nacht Nacht"))
+  });
+  c.bench_function("compares Night Night Night and Nacht Nacht Nacht", |b| {
+    b.iter(|| compare_two_strings("Night Night Night", "Nacht Nacht Nacht"))
+  });
+  c.bench_function("find_best_match Night and [Nacht, Night, Nacht]", |b| {
+    b.iter(|| find_best_match("Night", vec!["Nacht", "Night", "Nacht"]))
+  });
+  c.bench_function(
+    "find_best_match Night Night Night and [Nacht Nacht Nacht, Night Night Night, Night]",
+    |b| {
+      b.iter(|| {
+        find_best_match(
+          "Night Night Night",
+          vec!["Nacht Nacht Nacht", "Night Night Night", "Night"],
+        )
+      })
+    },
+  );
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,113 @@
-use std::collections::BTreeMap;
+use std::cmp::{self, min};
 
 pub struct MatchResult {
-    pub score: f64,
-    pub string: String,
+  pub score: f64,
+  pub string: String,
 }
 
 pub struct BestMatchResult {
-    pub best_result_index: usize,
-    pub result: Vec<MatchResult>,
-}
-
-#[inline]
-fn remove_whitespace(s: &str) -> String {
-    s.chars().filter(|c| !c.is_whitespace()).collect()
+  pub best_result_index: usize,
+  pub result: Vec<MatchResult>,
 }
 
 #[inline]
 pub fn compare_two_strings(first: &str, second: &str) -> f64 {
-    let first = remove_whitespace(first);
-    let second = remove_whitespace(second);
+  let first_len = first.len();
+  let second_len = second.len();
+  let mut d = vec![0; (first_len + 1) * (second_len + 1)];
 
-    if first == second {
-        return 1.0;
+  for i in 0..=first_len {
+    d[i * (second_len + 1)] = i;
+  }
+  for j in 0..=second_len {
+    d[j] = j;
+  }
+
+  for i in 1..=first_len {
+    for j in 1..=second_len {
+      let cost = if first.get(i - 1..i) == second.get(j - 1..j) {
+        0
+      } else {
+        1
+      };
+      d[i * (second_len + 1) + j] = min(
+        d[(i - 1) * (second_len + 1) + j] + 1,
+        min(
+          d[i * (second_len + 1) + j - 1] + 1,
+          d[(i - 1) * (second_len + 1) + j - 1] + cost,
+        ),
+      );
+      if i > 1
+        && j > 1
+        && first.get(i - 1..i) == second.get(j - 2..j - 1)
+        && first.get(i - 2..i - 1) == second.get(j - 1..j)
+      {
+        d[i * (second_len + 1) + j] = min(
+          d[i * (second_len + 1) + j],
+          d[(i - 2) * (second_len + 1) + j - 2] + 1,
+        );
+      }
     }
+  }
 
-    let first_len = first.len();
-    let second_len = second.len();
-
-    if first_len < 2 || second_len < 2 {
-        return 0.0;
-    }
-
-    let mut first_val: BTreeMap<&str, i32> = BTreeMap::new();
-
-    for i in 0..first_len - 1 {
-        let val = first.get(i..i + 2).unwrap();
-        let count = first_val.get(val).unwrap_or(&0) + 1;
-        first_val.insert(val, count);
-    }
-
-    let mut intersection_size: f64 = 0.0;
-    for i in 0..second_len - 1 {
-        let val = second.get(i..i + 2).unwrap();
-        let count: i32 = *first_val.get(val).unwrap_or(&0);
-        if count > 0 {
-            intersection_size += 1.0;
-            first_val.insert(val, count - 1);
-        }
-    }
-
-    (2.0 * intersection_size) / (first_len + second_len - 2) as f64
+  1.0
+    - (d[first_len * (second_len + 1) + second_len] as f64) / cmp::max(first_len, second_len) as f64
 }
 
 #[inline]
 pub fn find_best_match(string: &str, arr: Vec<&str>) -> BestMatchResult {
-    let mut result: Vec<MatchResult> = vec![];
-    let mut best_result_index: usize = 0;
+  let mut result: Vec<MatchResult> = vec![];
+  let mut best_result_index: usize = 0;
 
-    for (index, item) in arr.iter().enumerate() {
-        let score = compare_two_strings(string, item);
+  for (index, item) in arr.iter().enumerate() {
+    let score = compare_two_strings(string, item);
 
-        result.push(MatchResult {
-            score,
-            string: item.to_string(),
-        });
+    result.push(MatchResult {
+      score,
+      string: item.to_string(),
+    });
 
-        if score > result[best_result_index].score {
-            best_result_index = index;
-        }
+    if score > result[best_result_index].score {
+      best_result_index = index;
     }
+  }
 
-    BestMatchResult {
-        best_result_index,
-        result,
-    }
+  BestMatchResult {
+    best_result_index,
+    result,
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{compare_two_strings, find_best_match};
+  use super::{compare_two_strings, find_best_match};
 
-    #[test]
-    fn check_compare() {
-        let result: f64 = compare_two_strings("Night", "Nacht");
-        assert_eq!(result, 0.25);
-        assert_ne!(result, 0.5);
-    }
+  #[test]
+  fn check_compare() {
+    let result: f64 = compare_two_strings("Night", "Nacht");
+    assert_eq!(result, 0.6);
+    let result: f64 = compare_two_strings("Night Night Night", "Night Night Night");
+    assert_eq!(result, 1.0);
+    let result: f64 = compare_two_strings("Night Night Night", "Nacht Nacht Nacht");
+    assert_eq!(result, 0.6470588235294117);
+  }
 
-    #[test]
-    fn check_compare_space() {
-        let result: f64 = compare_two_strings("Night Night Night", "Nacht Nacht Nacht");
-        assert_eq!(result, 0.35714285714285715);
-        assert_ne!(result, 0.5);
-    }
-
-    #[test]
-    fn check_best_match() {
-        let result = find_best_match("Night", vec!["Nacht", "Night", "Nacht"]);
-        assert_eq!(result.result.len(), 3);
-        assert_eq!(result.result[0].score, 0.25);
-        assert_eq!(result.result[1].score, 1.0);
-        assert_eq!(result.result[2].score, 0.25);
-        assert_eq!(result.best_result_index, 1);
-    }
-
-    #[test]
-    fn check_best_match_space() {
-        let result = find_best_match(
-            "Night Night Night",
-            vec!["Nacht Nacht Nacht", "Night Night Night", "Night"],
-        );
-        assert_eq!(result.result.len(), 3);
-        assert_eq!(result.result[0].score, 0.35714285714285715);
-        assert_eq!(result.result[1].score, 1.0);
-        assert_eq!(result.result[2].score, 0.44444444444444444);
-        assert_eq!(result.best_result_index, 1);
-    }
+  #[test]
+  fn check_best_match() {
+    let result = find_best_match("Night", vec!["Nacht", "Night", "Nacht"]);
+    assert_eq!(result.result.len(), 3);
+    assert_eq!(result.result[0].score, 0.6);
+    assert_eq!(result.result[1].score, 1.0);
+    assert_eq!(result.result[2].score, 0.6);
+    assert_eq!(result.best_result_index, 1);
+    let result = find_best_match(
+      "Night Night Night",
+      vec!["Nacht Nacht Nacht", "Night Night Night", "Night"],
+    );
+    assert_eq!(result.result.len(), 3);
+    assert_eq!(result.result[0].score, 0.6470588235294117);
+    assert_eq!(result.result[1].score, 1.0);
+    assert_eq!(result.result[2].score, 0.2941176470588235);
+    assert_eq!(result.best_result_index, 1);
+  }
 }
